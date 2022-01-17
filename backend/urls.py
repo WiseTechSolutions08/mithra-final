0c7272752cb85acd6d2423b227680abac2818bd1
vinoth.kr <vinoth.kr@zohocorp.com>
2022-01-18 00:59:55 +0530

    repath issue fix

--- backend/urls.py
+++ backend/urls.py
@@ -29,9 +29,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    re_path('.*',TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-auth/', include('drf_social_oauth2.urls', namespace='drf')),
